803acb354b213bff1324658c1c729d0edb33e7ab
Julian Hoever <julianhoever@posteo.de>
2026-03-10 16:20:37 +0100

    adopt code to lecture
    Signed-off-by: Julian Hoever <julianhoever@posteo.de>

--- lecture_3/blink/blink.c
+++ lecture_3/blink/blink.c
@@ -3,14 +3,17 @@
 #define LED_PIN 25
 #define LED_DELAY_MS 250
 
+
 void led_init(void) {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 0);
 }
 
 int main(void) {
     led_init();
-    while (1) {
+    
+    while (true) {
         gpio_put(LED_PIN, 1);
         sleep_ms(LED_DELAY_MS);
         gpio_put(LED_PIN, 0);
